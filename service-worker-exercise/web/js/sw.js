706d919f680aca9e70f31906bfc85cf318771770
sssmi <simeon.kerkola@gmail.com>
2019-10-02 08:40:06 +0300

    First service worker running

--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,4 +1,30 @@
 "use strict";
 
-// TODO
+// Making sure the sw gets updated after changes
+const version = 1;
 
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+
+main().catch(console.error);
+
+// *********************************
+
+async function main() {
+  console.log(`Service worker (${version}) is starting...`);
+}
+
+async function onInstall() {
+  console.log(`Service worker (${version}) is installed.`);
+  self.skipWaiting();
+}
+
+function onActivate(e) {
+  // Tell the browser not to shut us down, ie if the user leaves the site, while still activating
+  e.waitUntil(handleActivation());
+}
+async function handleActivation() {
+  // Claim all the open clients (ie. multiple tabs of the site)
+  await clients.claim();
+  console.log(`Service worker (${version}) is activated.`);
+}
